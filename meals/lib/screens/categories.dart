4ce802af902fd79bb1db3b60e0aa7016e66f87d1
praty474 <p.kayastha.pk@gmail.com>
2023-07-27 23:54:12 +0545

    09 commit

--- meals/lib/screens/categories.dart
+++ meals/lib/screens/categories.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: 'Meals',
+          meals: [],
+        ),
+      ),
+    ); //Navigator.push(context,route)
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
